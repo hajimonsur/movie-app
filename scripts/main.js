b22eb900a59d446bb747805ad67ca75b2294decf
hajimonsur <taiwomonsuru29@gmail.com>
2024-11-23 18:42:13 +0100

    removed unwanted comments

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,9 @@ const fetchMovies = async () => {
     const url = 'https://api.themoviedb.org/3/movie/popular?api_key=385fb5fb9f8cebd4e1924b27b84ba8a3';
     const options = {
         method: 'GET',
-    //     headers: {
-    //         'x-rapidapi-key': 'dee6c30cd2msh0cdb20a9aa42f73p1abefcjsna1281eba48ac',
-    //         'x-rapidapi-host': 'job-search-api1.p.rapidapi.com'
-    //     }
+        headers: {
+            accept: 'application/json',
+        },
     };
     
     try {
